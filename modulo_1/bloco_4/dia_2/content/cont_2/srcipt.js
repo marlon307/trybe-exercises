a2469b2cfe0f5f56935dcb88782b69b9a8e730c0
marlon307 <marlon-ramosb@hotmail.com>
2021-03-29 09:44:59 -0300

    modulo_1 - bloco_4 - dia_2

---
+++ modulo_1/bloco_4/dia_2/content/cont_2/srcipt.js
@@ -0,0 +1,27 @@
+//Obtenha o valor "Serviços" do array menu :
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+console.log(menuServices);
+
+// Procure o índice do valor "Portfólio" do array menu :
+
+let menuService = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+let indexOfPortfolio;
+
+for (var i = 0; i < menuService.length; i++){
+    if (menuService[i] == 'Portfólio') {
+        indexOfPortfolio = i;
+    }
+}
+
+console.log(indexOfPortfolio);
+
+
+// Adicione o valor "Contato" no final do array menu :
+
+menuService.push('Contato');
+
+console.log(menuService);
---
+++ modulo_1/bloco_4/dia_2/content/cont_3/script.js
@@ -0,0 +1,5 @@
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let n of names) {
+    console.log(n);
+}
