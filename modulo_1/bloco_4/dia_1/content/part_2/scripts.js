17f3b85357432d019233b46bbc7ba74811752041
marlon307 <marlon-ramosb@hotmail.com>
2021-03-29 09:40:07 -0300

    modulo_1 - bloco_4 - dia_1

---
+++ modulo_1/bloco_4/dia_1/content/part_2/scripts.js
@@ -0,0 +1,16 @@
+const name = 'Marlon Ramos';
+
+const birthCity = 'Coronel Fabricinao';
+
+var birthYear = 1996;
+
+console.log(name, birthCity, birthYear);
+
+birthYear = 2021;
+
+console.log(birthYear);
+
+birthCity = 'Serra - ES';
+
+// Esta dando erro pois é uma variável constante e não pode ser alterada.
+console.log(birthCity);
---
+++ modulo_1/bloco_4/dia_1/content/part_5/script.js
@@ -0,0 +1,12 @@
+var lista = true;
+
+switch (lista){
+    case true:
+        console.log('Aprovada');
+        break;
+    case false:
+        console.log('Reprovada');
+        break;
+    default:
+        console.log('Não se aplica');
+}
