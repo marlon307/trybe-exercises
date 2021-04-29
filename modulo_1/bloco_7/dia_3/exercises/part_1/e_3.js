f97432480be528f66be36a9be1d9e39ae372b8ad
marlon307 <marlon-ramosb@hotmail.com>
2021-04-29 07:53:22 -0300

    modulo_1-bloco_7-dia_e - execises

---
+++ modulo_1/bloco_7/dia_3/exercises/part_1/e_3.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+  return arr;
+}
+
+// implemente seus testes aqui
+let arr = [1, 2, 3, 4];
+
+console.log(myRemoveWithoutCopy(arr, 4));
