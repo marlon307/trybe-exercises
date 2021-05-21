593773a100b184ee394254cc70fa06ff661c3d4b
marlon307 <marlon-ramosb@hotmail.com>
2021-05-21 13:34:14 -0300

    modulo_1-bloco_10-dia_3 update 5

--- modulo_1/bloco_10/dia_3/exercises/script.test.js
+++ modulo_1/bloco_10/dia_3/exercises/script.test.js
@@ -6,17 +6,14 @@ describe('Teste Unitario', () => {
   test('Exercise 1', () => {
     randNumber = jest.fn().mockReturnValue(10);
 
-    randNumber();
-    expect(randNumber).toHaveBeenCalled();
     expect(randNumber()).toBe(10);
+    expect(randNumber).toHaveBeenCalled();
   })
   test('Exercise 2', () => {
-    randNumber = jest.fn().mockImplementation((a, b) => a / b);
-
-    randNumber();
+    randNumber = jest.fn().mockImplementationOnce((a, b) => a / b);
+    expect(randNumber(6, 2)).toBe(3);
     expect(randNumber).toHaveBeenCalled();
     expect(randNumber).toHaveBeenCalledTimes(1);
-    expect(randNumber(6, 2)).toBe(3);
   })
   test('Exercise 3', () => {
     randNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
@@ -24,10 +21,10 @@ describe('Teste Unitario', () => {
 
     randNumber.mockReset();
     randNumber = jest.fn().mockReturnValue(10);
-    randNumber();
+
+    expect(randNumber()).toBe(10);
     expect(randNumber).toHaveBeenCalled();
     expect(randNumber).toHaveBeenCalledTimes(1);
-    expect(randNumber()).toBe(10);
   })
   test('Exercise 4', () => {
     uppercaseLetter = jest.fn().mockImplementation((str) => str.toLowerCase());
@@ -40,13 +37,7 @@ describe('Teste Unitario', () => {
     expect(concatString('teste1', 'teste2', 'teste3')).toBe('teste1 teste2 teste3');
   })
   test('Exercise 5', () => {
-    uppercaseLetter = jest.fn().mockImplementation((str) => str.toLowerCase());
-    expect(uppercaseLetter('TESTE')).toBe('teste');
-
-    firsrtUppcLetter = jest.fn().mockImplementation((str) => str[str.length - 1]);
-    expect(firsrtUppcLetter('teste')).toBe('e');
-
-    concatString = jest.fn().mockImplementation((param1, param2, param3) => `${param1} ${param2} ${param3}`);
-    expect(concatString('teste1', 'teste2', 'teste3')).toBe('teste1 teste2 teste3');
+    randNumber.mockReset();
+    expect(randNumber())
   })
 });
