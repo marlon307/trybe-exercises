810627582a00d1e1fa2511eaca242f1d0817e040
marlon307 <marlon-ramosb@hotmail.com>
2021-06-22 15:52:32 -0300

    modulo_2-bloco_13-dia_2

---
+++ modulo_2/bloco_13/dia_2/my_app-content/src/components/Users.js
@@ -0,0 +1,19 @@
+// arquivo Users.js
+import React, { Component } from 'react';
+
+class Users extends Component {
+  render() {
+    const { greetingsMessage } = this.props
+    const { id } = this.props.match.params;
+    console.log(greetingsMessage);
+    return (
+      <div>
+        <h2>Users</h2>
+        <p> { greetingsMessage }, My awesome Users component { id }</p>
+      </div>
+    );
+  }
+};
+
+export default Users;
+
