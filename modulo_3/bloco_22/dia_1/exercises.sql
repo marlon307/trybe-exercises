d4ce73d539f0bd4c5b763c5af282aa9f12e810f0
marlon307 <marlon-ramosb@hotmail.com>
2021-09-08 18:59:22 -0300

    modulo_3-bloco_22-dia_1-Exercises-7-8

--- modulo_3/bloco_22/dia_1/exercises.sql
+++ modulo_3/bloco_22/dia_1/exercises.sql
@@ -2,15 +2,22 @@
 
 
 -- Exercício 1 : Retorne o documento com o _id igual a 8.
-db.bios.find({_id: 8});
+db.bios.find({ _id: 8 });
 -- Exercício 2 : Retorne o documento com o _id igual a 8, mas só exiba os atributos: _id e name .
-db.bios.find({_id: 8}, {_id: 1, name: 1});
+db.bios.find({ _id: 8 }, { _id: 1, name: 1 });
 -- Exercício 3 : Retorne apenas os atributos name e birth do documento com o _id igual a 8.
-db.bios.find({_id: 8}, {_id: 0, name: 1, birth: 1});
+db.bios.find({ _id: 8}, { _id: 0, name: 1, birth: 1 });
 -- Exercício 4 : Retorne todos os documentos em que o atributo name.first seja igual a John , utilizando o método pretty() .
-db.bios.find({ 'name.first': 'John'}).pretty();
+db.bios.find({ 'name.first': 'John' }).pretty();
 -- Exercício 5 : Retorne os 3 primeiros documentos da coleção bios utilizando o método pretty() .
 db.bios.find().limit(3).pretty();
 -- Exercício 6 : Retorne 2 documentos da coleção bios pulando os 5 primeiros documentos.
 db.bios.find().limit(2).skip(5).pretty();
+
+
+-- mongoimport --db=listbooks --collection=books --file=books-48d15e4d8924badc2308cc4a62eb3ea4.json
+
 -- Exercício 7 : Retorne a quantidade de documentos da coleção books .
+db.books.count();
+-- Exercício 8 : Conte quantos livros existem com o status = "PUBLISH" .
+db.books.find({ status: "PUBLISH" }).count();
