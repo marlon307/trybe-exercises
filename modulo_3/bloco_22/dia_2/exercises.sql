fc0a8cfa928b967dbfe2177034251e2ef2003b11
marlon307 <marlon-ramosb@hotmail.com>
2021-09-10 07:58:12 -0300

    modulo_3-bloco_22-dia_2-Exercises-9

--- modulo_3/bloco_22/dia_2/exercises.sql
+++ modulo_3/bloco_22/dia_2/exercises.sql
@@ -1,17 +1,18 @@
 -- Exercício 1: Inspecione um documento para que você se familiarize com a estrutura. Entenda os atributos e os níveis existentes.
 db.superheroes.count();
 -- Exercício 2: Selecione todos os super-heróis com menos de 1.80m de altura. Lembre-se de que essa informação está em centímetros.
-db.superheroes.find({ 'aspects.height': { $lt: 180 }});
+db.superheroes.find({ 'aspects.height': { $lt: 180 } });
 --Exercício 3: Retorne o total de super-heróis menores que 1.80m.
-db.superheroes.find({ 'aspects.height': { $lt: 180 }}).count();
+db.superheroes.find({ 'aspects.height': { $lt: 180 } }).count();
 -- Exercício 4: Retorne o total de super-heróis com até 1.80m.
-db.superheroes.find({ 'aspects.height': { $lte: 180 }});
+db.superheroes.find({ 'aspects.height': { $lte: 180 } });
 -- Exercício 5: Selecione um super-herói com 2.00m ou mais de altura.
-db.superheroes.find({ 'aspects.height': { $gte: 200 }}).limit(2);
+db.superheroes.find({ 'aspects.height': { $gte: 200 } }).limit(2);
 -- Exercício 6: Retorne o total de super-heróis com 2.00m ou mais.
-db.superheroes.find({ 'aspects.height': { $gte: 200 }}).count();
+db.superheroes.find({ 'aspects.height': { $gte: 200 } }).count();
 -- Exercício 7: Selecione todos os super-heróis que têm olhos verdes.
-db.superheroes.find({ 'aspects.eyeColor': { $eq: 'green' }});
+db.superheroes.find({ 'aspects.eyeColor': { $eq: 'green' } });
 -- Exercício 8: Retorne o total de super-heróis com olhos azuis.
-db.superheroes.find({ 'aspects.eyeColor': { $eq: 'blue' }}).count();
+db.superheroes.find({ 'aspects.eyeColor': { $eq: 'blue' } }).count();
 -- Exercício 9: Utilizando o operador $in , selecione todos os super-heróis com cabelos pretos ou carecas ( "No Hair" ).
+db.superheroes.find({ 'aspects.eyeColor': { $in: ['black', 'No Hair'] } });
