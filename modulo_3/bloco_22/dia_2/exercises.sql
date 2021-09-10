5122c71c94d38160be4fc74d6aa72a9219d1591a
marlon307 <marlon-ramosb@hotmail.com>
2021-09-10 07:46:22 -0300

    modulo_3-bloco_22-dia_2-Exercises-3

--- modulo_3/bloco_22/dia_2/exercises.sql
+++ modulo_3/bloco_22/dia_2/exercises.sql
@@ -2,3 +2,5 @@
 db.superheroes.count();
 -- Exercício 2: Selecione todos os super-heróis com menos de 1.80m de altura. Lembre-se de que essa informação está em centímetros.
 db.superheroes.find({ 'aspects.height': { $lt: 180 }});
+--Exercício 3: Retorne o total de super-heróis menores que 1.80m.
+db.superheroes.find({ 'aspects.height': { $lt: 180 }}).count();
