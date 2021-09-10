c8aa145c05747f08f4d768a7e3325b916f0d63cc
marlon307 <marlon-ramosb@hotmail.com>
2021-09-10 07:51:41 -0300

    modulo_3-bloco_22-dia_2-Exercises-5

--- modulo_3/bloco_22/dia_2/exercises.sql
+++ modulo_3/bloco_22/dia_2/exercises.sql
@@ -6,3 +6,5 @@ db.superheroes.find({ 'aspects.height': { $lt: 180 }});
 db.superheroes.find({ 'aspects.height': { $lt: 180 }}).count();
 -- Exercício 4: Retorne o total de super-heróis com até 1.80m.
 db.superheroes.find({ 'aspects.height': { $lte: 180 }});
+-- Exercício 5: Selecione um super-herói com 2.00m ou mais de altura.
+db.superheroes.find({ 'aspects.height': { $gte: 200 }}).limit(2);
