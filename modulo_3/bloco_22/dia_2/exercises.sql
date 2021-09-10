7cd46838c9f33235f5ef172ccde51708859edd8c
marlon307 <marlon-ramosb@hotmail.com>
2021-09-10 07:47:59 -0300

    modulo_3-bloco_22-dia_2-Exercises-4

--- modulo_3/bloco_22/dia_2/exercises.sql
+++ modulo_3/bloco_22/dia_2/exercises.sql
@@ -4,3 +4,5 @@ db.superheroes.count();
 db.superheroes.find({ 'aspects.height': { $lt: 180 }});
 --Exercício 3: Retorne o total de super-heróis menores que 1.80m.
 db.superheroes.find({ 'aspects.height': { $lt: 180 }}).count();
+-- Exercício 4: Retorne o total de super-heróis com até 1.80m.
+db.superheroes.find({ 'aspects.height': { $gt: 180 }});
