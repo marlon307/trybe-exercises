a10084e3360760fc723b28d9332da4d0f763de1e
marlon307 <marlon-ramosb@hotmail.com>
2021-09-14 18:45:57 -0300

    modulo_3-bloco_24-dia_2-Exercises-up

--- modulo_3/bloco_24/dia_2/exercises.sql
+++ modulo_3/bloco_24/dia_2/exercises.sql
@@ -1,3 +1,34 @@
+db.movies.drop();
+db.movies.insertMany([
+  {
+    title: "Batman",
+    category: [
+      "action",
+      "adventure",
+    ],
+    imdbRating: 7.7,
+    budget: 35,
+  },
+  {
+    title: "Godzilla",
+    category: [
+      "action",
+      "adventure",
+      "sci-fi",
+    ],
+    imdbRating: 6.6,
+    budget: 1,
+  },
+  {
+    title: "Home Alone",
+    category: [
+      "family",
+      "comedy",
+    ],
+    imdbRating: 7.4,
+  },
+]);
+
 -- Exercício 1: Adicione a categoria "superhero" ao filme Batman .
 db.movies.updateMany({
   title: "Batman"
@@ -46,6 +77,78 @@ db.movies.updateMany({
 }, {
   $pop: { 
     category: 1
+  }
+});
+
+-- Exercício 6: Adicione o elemento "action" ao array category do filme Batman , garantindo que esse valor não se duplique.
+
+db.movies.updateMany({
+  title: "Batman"
+}, {
+  $addToSet: { 
+    category: "action"
+  }
+});
+
+-- Exercício 7: Adicione a categoria "90's" aos filmes Batman e Home Alone .
+
+db.movies.updateMany({
+  title: { $in:  ["Batman", "Home Alone"] }
+}, {
+  $addToSet: { 
+    category: "90's"
+  }
+});
+
+-- Exercício 8: Crie um array de documentos chamado cast para o filme Home Alone com os
+
+db.movies.updateMany({
+  title: "Home Alone",
+}, {
+  $set: { 
+    cast: [
+      {
+        "actor": "Macaulay Culkin",
+        "character": "Kevin"
+      },
+      {
+        "actor": "Joe Pesci",
+        "character": "Harry"
+      },
+      {
+        "actor": "Daniel Stern"
+      }
+    ]
+  }
+});
+
+-- Exercício 9: Adicione o campo character com o valor Marv ao array de cast em que o campo actor seja igual a Daniel Stern no filme Home Alone .
+
+db.movies.updateMany({
+  title: "Home Alone",
+}, {
+  $set: { 
+    "cast.2.character": "Marv"
+  }
+});
+
+-- Exercício 10: Crie um array de documentos chamado cast para o filme Batman com os seguintes dados:
+
+db.movies.updateMany({
+  title: "Batman",
+}, {
+  $set: { 
+    cast: [
+      {
+        "character": "Batman"
+      },
+      {
+        "character": "Alfred"
+      },
+      {
+        "character": "Coringa"
+      }
+    ]
   }
 });
 
