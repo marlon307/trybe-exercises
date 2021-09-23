7b0d2a74c9d89a2d9e8fd8358969412862d8ad47
marlon307 <marlon-ramosb@hotmail.com>
2021-09-23 18:23:17 -0300

    modulo_3-bloco_26-dia_1 update 6

--- modulo_3/bloco_26/dia_1/icms.js
+++ modulo_3/bloco_26/dia_1/icms.js
@@ -3,7 +3,8 @@ const readline = require('readline-sync');
 const altura = readline.questionInt('Altura?');
 const peso = readline.questionFloat('Peso?');
 
-const icms = () => peso / (altura * 2);
+const icms = () => peso / Math.pow(altura / 100, 2).toFixed(2);
+
 
 module.exports = {
   icms
---
+++ modulo_3/bloco_26/dia_1/velocidade.js
@@ -0,0 +1,9 @@
+const readline = require('readline-sync');
+
+const distancia = readline.questionInt('Qual a distancia? ');
+const tempo = readline.questionInt('Qual o tempo? ');
+
+const velocidade = () => distancia / tempo;
+
+
+console.log('Velocidade ' + velocidade());
