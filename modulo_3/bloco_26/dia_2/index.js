2d601e4b2fd3f6ab0c79a113dd98ca284f72c4e7
marlon307 <marlon-ramosb@hotmail.com>
2021-09-27 10:57:21 -0300

    modulo_3-bloco_26-dia_2 exercise2

--- modulo_3/bloco_26/dia_2/index.js
+++ modulo_3/bloco_26/dia_2/index.js
@@ -1,7 +1,10 @@
 const { exercise1 } = require('./exercise1');
 
-async function teste() {
-  console.log(await exercise1(2, 15, 3));
-}
 
-teste()
+const random1 = Math.floor(Math.random() * 100 + 1);
+const random2 = Math.floor(Math.random() * 100 + 1);
+const random3 = Math.floor(Math.random() * 100 + 1);
+
+exercise1(random1, random2, random3)
+  .then((result) => console.log(`sucesso ${result}`))
+  .catch((err) => console.log(`error ${err.message}`));
