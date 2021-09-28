aaadeeee01374f2eb82481ad3f8e888e3322e90c
marlon307 <marlon-ramosb@hotmail.com>
2021-09-28 14:52:33 -0300

    modulo_3-bloco_26-dia_3 exercises 5 up1

--- modulo_3/bloco_26/dia_3/tests/test5.js
+++ modulo_3/bloco_26/dia_3/tests/test5.js
@@ -2,32 +2,51 @@ const fs = require('fs');
 const sinon = require('sinon');
 const { expect } = require('chai');
 
+const resultRequired = 'texto';
 const { textwriteFile, lerArquivo } = require('../exercise4');
 
-const resultRequired = 'texto';
+describe('Exercises', () => {
+  describe('Exercise 4', () => {
 
-
-describe('Quando o arquivo não existe', () => {
-  before(() => {
-    sinon.stub(fs, 'writeFileSync')
-      .throws(new Error('Arquivo não encontrado'));
-  });
-
-  after(() => {
-    fs.writeFileSync.restore();
-  });
-
-  describe('a resposta é', () => {
-    it('é uma "string"', () => {
-      const resposta = textwriteFile('text.txt', resultRequired);
-
-      expect(resposta).to.be.a('string');
+    before(() => {
+      sinon.stub(fs, 'writeFileSync')
+        .throws(new Error('Arquivo não encontrado'));
     });
 
-    it('igual a "null"', () => {
-      const resulRead = lerArquivo('arquivo_que_nao_existe.txt');
+    after(() => {
+      fs.writeFileSync.restore();
+    });
 
-      expect(resulRead).to.be.equal(null);
+    describe('Resultado', () => {
+      it('Escreva ok', () => {
+        const write = textwriteFile('text.txt', resultRequired);
+        expect(write).to.be.a('string')
+      })
+
+      it('Return ok', () => {
+        const write = textwriteFile('text.txt', resultRequired);
+        expect(write).to.be.equals('ok')
+      })
+    })
+  });
+
+  describe('Exercise 5', () => {
+
+    before(() => {
+      sinon.stub(fs, 'readFileSync')
+        .throws(new Error('Arquivo não encontrado'));
+    });
+
+    after(() => {
+      fs.readFileSync.restore();
+    });
+
+    describe('Resultado', () => {
+      it('deve ser igual a "null"', () => {
+        const resulRead = lerArquivo('arquivo_que_nao_existe.txt');
+
+        expect(resulRead).to.be.equal(null);
+      })
     })
   })
 });
