9659cb91aced56b6ce8ab9841d97942c2ba61d11
marlon307 <marlon-ramosb@hotmail.com>
2021-10-09 09:57:10 -0300

    modulo_3-bloco_26-7-dia_1 exercise 1

---
+++ modulo_3/bloco_26-7/dia_1/mongo/getAll.js
@@ -0,0 +1,16 @@
+const connection = require('./connectionMongo')
+
+const getAll = async () =>
+  await connection()
+  .then((db) => db.collection('authors')
+    .find().toArray())
+  .then((authors) =>
+    authors.map((_id, firstName, middleName, lastName) => ({
+      id: _id,
+      firstName,
+      middleName,
+      lastName,
+    }))
+  )
+
+module.exports = getAll;
