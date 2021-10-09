7df387b8d3d44b71bb8d1dcfe1507203f166c867
marlon307 <marlon-ramosb@hotmail.com>
2021-10-09 10:17:17 -0300

    modulo_3-bloco_26-7-dia_1 exercise 2

--- modulo_3/bloco_26-7/dia_1/mongo.js
+++ modulo_3/bloco_26-7/dia_1/mongo.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const getAll = require('./mongo/getAll');
+const getID = require('./mongo/getID');
 
 const app = express();
 
@@ -10,6 +11,12 @@ app.use(cors())
 
 app.get('/', async (req, res) => {
   const results = await getAll()
+  res.status(202).json(results);
+});
+
+app.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const results = await getID(id);
   res.status(202).json(results);
 });
 
