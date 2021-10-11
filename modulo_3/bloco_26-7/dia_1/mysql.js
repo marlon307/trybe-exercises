7cd45407096589e5988047f0cda36a39508d221c
marlon307 <marlon-ramosb@hotmail.com>
2021-10-11 06:50:52 -0300

    modulo_3-bloco_26-7-dia_1 exercise Mysql 2

--- modulo_3/bloco_26-7/dia_1/mysql.js
+++ modulo_3/bloco_26-7/dia_1/mysql.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const getAll = require('./mysql/getAll');
+const getID = require('./mysql/getID');
 
 
 const app = express();
 
-app.get('/', async (req, res) => {
+app.get('/authors', async (_req, res) => {
   const result = await getAll()
+  res.status(202).json(result);
+})
+
+app.get('/authors/:id', async (req, res) => {
+  const { id } = req.params;
+  const result = await getID(id);
   res.status(202).json(result);
 })
 
