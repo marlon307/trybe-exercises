1314e9bec5f77424fa0eb65cf199df22dbd4776c
marlon307 <marlon-ramosb@hotmail.com>
2021-10-11 08:01:13 -0300

    modulo_3-bloco_26-7-dia_1 exercise Mysql 3

--- modulo_3/bloco_26-7/dia_1/mysql.js
+++ modulo_3/bloco_26-7/dia_1/mysql.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const isValid = require('./mysql/isValid');
 const getAll = require('./mysql/getAll');
 const getID = require('./mysql/getID');
+const putAuthor = require('./mysql/putAuthor');
 
 
 const app = express();
+app.use(express.json());
 
 app.get('/authors', async (_req, res) => {
   const result = await getAll()
@@ -14,6 +17,17 @@ app.get('/authors/:id', async (req, res) => {
   const { id } = req.params;
   const result = await getID(id);
   res.status(202).json(result);
+})
+
+app.post('/authors', async (req, res) => {
+  const { firstName, middleName, lastName } = req.body;
+
+  if (!isValid(firstName, middleName, lastName)) {
+    return res.status(400).json({ message: 'Dados inválidos' });
+  }
+
+  await putAuthor(firstName, middleName, lastName);
+  res.status(202).json({ message: 'Autor criado com sucesso! ' });
 })
 
 app.use(express())
