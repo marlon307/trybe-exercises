d9165937fb65affe829f478441d5cb81bdfb5d9a
marlon307 <marlon-ramosb@hotmail.com>
2021-10-11 06:36:50 -0300

    modulo_3-bloco_26-7-dia_1 exercise Mysql 1

--- modulo_3/bloco_26-7/dia_1/mysql.js
+++ modulo_3/bloco_26-7/dia_1/mysql.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const getAll = require('./mysql/getAll');
+
 
 const app = express();
 
+app.get('/', async (req, res) => {
+  const result = await getAll()
+  res.status(202).json(result);
+})
+
 app.use(express())
+
+app.listen(3000, () => {
+  console.log('http://localhost:3000/');
+});
