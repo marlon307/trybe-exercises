aa900c38606d55d06775b54b87c69161bdd32752
marlon307 <marlon-ramosb@hotmail.com>
2021-12-02 09:42:37 -0300

    Content update soket chat

--- modulo_3/bloco_30/dia_2/content/client.js
+++ modulo_3/bloco_30/dia_2/content/client.js
@@ -1,21 +1,15 @@
 const net = require('net');
+const client = new net.Socket();
 const stdin = process.openStdin();
 
-const client = net.connect({ port: 8080 }, () => {
-  console.log('Cliente conectado ao servidor!');
+client.connect(8080, 'localhost', () => {
   stdin.addListener('data', (text) => {
-    const message = text.toString().trim();
+    const message = text.toString().trim()
+
     client.write(message);
-  })
+  });
 });
 
 client.on('data', (data) => {
-  const text = data.toString();
-  if (text === '/exit') {
-    client.end();
-  }
-});
-
-client.on('end', () => {
-  console.log('Desconectado do servidor');
+  console.log(data.toString());
 });
