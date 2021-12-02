6e69d325d6fd05d6d6afd047af8040b661ffcc5b
marlon307 <marlon-ramosb@hotmail.com>
2021-12-02 16:18:22 -0300

    modulo_3-bloco_30-dia_3 content chat not found

--- modulo_3/bloco_30/dia_3/content/index.js
+++ modulo_3/bloco_30/dia_3/content/index.js
@@ -4,15 +4,17 @@ const http = require('http').createServer(app);
 
 const io = require('socket.io')(http, {
   cors: {
-    origin: 'http://localhost:3000', // url aceita pelo cors
+    origin: 'http://localhost:3000/', // url aceita pelo cors
     methods: ['GET', 'POST'], // Métodos aceitos pela url
   }
 });
 
 require('./sockets/ping')(io);
 
+require('./sockets/chat')(io);
+
 app.use(express.static(__dirname + '/public'));
 
 http.listen(3000, () => {
-  console.log('Servidor ouvindo na porta 3000');
+  console.log('Servidor ouvindo na porta http://localhost:3000');
 });
