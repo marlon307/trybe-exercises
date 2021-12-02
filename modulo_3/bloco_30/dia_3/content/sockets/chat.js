326708e188590c989e0336bf275c256923a2e4c9
marlon307 <marlon-ramosb@hotmail.com>
2021-12-02 16:42:55 -0300

    modulo_3-bloco_30-dia_3 chat update not found

--- modulo_3/bloco_30/dia_3/content/sockets/chat.js
+++ modulo_3/bloco_30/dia_3/content/sockets/chat.js
@@ -1,4 +1,7 @@
 module.exports = (io) => io.on('connection', (socket) => {
+  socket.emit('serverMessage', 'Olá, seja bem vindo ao nosso chat público! Use essa página para conversar a vontade.');
+  socket.broadcast.emit('serverMessage', `Iiiiiirraaaa! ${socket.id} acabou de se conectar :D`);
+
   socket.on('clientMessage', (message) => {
     console.log(`Mensagem ${message}`);
     io.emit('serverMessage', message);
