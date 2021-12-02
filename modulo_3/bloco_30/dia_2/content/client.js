a8c0c794eaf2fb62084aaea467a436355ef90605
marlon307 <marlon-ramosb@hotmail.com>
2021-12-02 10:18:22 -0300

    Content update soket chat

--- modulo_3/bloco_30/dia_2/content/client.js
+++ modulo_3/bloco_30/dia_2/content/client.js
@@ -5,11 +5,11 @@ const stdin = process.openStdin();
 client.connect(8080, 'localhost', () => {
   stdin.addListener('data', (text) => {
     const message = text.toString().trim()
-
     client.write(message);
   });
 });
 
 client.on('data', (data) => {
-  console.log(data.toString());
+  const text = data.toString();
+  console.log(text);
 });
