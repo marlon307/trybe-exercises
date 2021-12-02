4aa86886d38690cec443a7759a99344221fab8b4
marlon307 <marlon-ramosb@hotmail.com>
2021-12-02 08:11:07 -0300

    Content update sokets

--- modulo_3/bloco_30/dia_2/content/client.js
+++ modulo_3/bloco_30/dia_2/content/client.js
@@ -1,16 +1,21 @@
 const net = require('net');
-/* Através do pacote NET, nós podemos não só criar servidores como podemos conectar nossos clientes aos servidores */
+const stdin = process.openStdin();
+
 const client = net.connect({ port: 8080 }, () => {
   console.log('Cliente conectado ao servidor!');
+  stdin.addListener('data', (text) => {
+    const message = text.toString().trim();
+    client.write(message);
+  })
 });
 
-/* Assim como no servidor, também temos eventos do lado do cliente, onde o evento 'data' é ativado quando o servidor envia uma mensagem para o cliente. */
 client.on('data', (data) => {
-  console.log(data.toString());
-  client.end();
+  const text = data.toString();
+  if (text === '/exit') {
+    client.end();
+  }
 });
 
-/* Quando a conexão é interrompida/terminada, é ativado o evento 'end', onde podemos limpar alguns caches, dar uma mensagem para usuário, atualizar algum dado no banco de dados etc. */
 client.on('end', () => {
   console.log('Desconectado do servidor');
 });
