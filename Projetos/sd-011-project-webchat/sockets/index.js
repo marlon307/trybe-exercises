ee8cfee92f2392b222a5aaa89099ed274fad3259
marlon307 <marlon-ramosb@hotmail.com>
2021-12-10 07:10:46 -0300

    project-web-chat update list nickname

--- Projetos/sd-011-project-webchat/sockets/index.js
+++ Projetos/sd-011-project-webchat/sockets/index.js
@@ -8,6 +8,7 @@ module.exports = (io) => {
   io.on('connection', (socket) => {
     const idsectionUser = socket.id.slice(0, 16);
     arrayUsersOnline.push({ section: idsectionUser, userNickName: idsectionUser });
+
     io.emit('usersOnline', { section: idsectionUser, userNickName: idsectionUser }); // para todos
     socket.emit('setmeOnline', { arrayUsersOnline, idsectionUser }); // Somente para o usuario que acabou de conectar
 
